93b70118f7043056356f696d47eb8bef77c9a5b6
Amadeo Abqary Dhiyaulhaq <deo531.id@gmail.com>
2025-09-10 13:48:55 +0700

    Update DriverTest.java

--- DriverTest.java
+++ DriverTest.java
@@ -1,8 +1,8 @@
 public class DriverTest {
     public static void main(String[] args) {
-        System.out.println("--------------------------");
-        Driver Guru1 = new Driver();
-        Guru1.Print();
-        System.out.println("--------------------------");
+        System.out.println("---------------");
+        Driver guru1 = new Driver("Bu Pasha", "Produktif RPL", 35, 2000000.0);
+        guru1.Print();
+        System.out.println("---------------");
     }
 }
